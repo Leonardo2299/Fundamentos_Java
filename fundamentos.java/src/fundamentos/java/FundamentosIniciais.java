a82e29b9c38e70a63d2b123b3f84b10559b5e53c
Léo <leonardo2299@gmail.com>
2022-10-05 16:08:25 -0300

    Implementação da classe FundamentosIniciais

---
+++ fundamentos.java/src/fundamentos/java/FundamentosIniciais.java
@@ -0,0 +1,15 @@
+package fundamentos.java;
+
+public class FundamentosIniciais {
+
+	public static void main(String[] args) {
+		System.out.println(
+				"Para executar um arquivo java no CMD do windows: \n"
+				+ "o nome da classe tem que ser o mesmo do arquivo. "
+				+ "Ex: public class Abc e Abc.java \n \n"
+				+ "Depois pelo CMD, ir até a pasta onde o arquivo está salvo "
+				+ "e dar os comandos: javac Abc.java e java Abc");
+
+	}
+
+}
