03c1420b13eba92674baaf775c7a2f12977487fc
Léo <leonardo2299@gmail.com>
2022-10-06 12:16:43 -0300

    implementacao da classe SequenciaDeEscape

---
+++ fundamentos.java/src/fundamentos/java/SequenciaDeEscape.java
@@ -0,0 +1,14 @@
+package fundamentos.java;
+
+public class SequenciaDeEscape {
+
+	public static void main(String[] args) {
+		System.out.println("Oi \"Maria\"");
+
+	    System.out.println("Seu nome:\nJoão");
+
+	    System.out.println("C:\\Windows");
+
+	}
+
+}
